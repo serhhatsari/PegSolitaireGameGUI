b7efc53a72aaac437849f1614625728d6b910cc6
serhhatsari <serhhatsari@gmail.com>
2022-01-29 14:47:59 +0300

    Peg Solitaire

--- PegGame/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"PegGame","l":"PegSolitaire"},{"p":"PegGame","l":"PegSolitaireInterface"}];updateSearchResults();
